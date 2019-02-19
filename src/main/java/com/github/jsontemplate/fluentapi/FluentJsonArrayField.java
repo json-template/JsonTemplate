bf9afc5cf7d9d457f7ebe2b8c5b72be59b3beb04
haihanyin <iamhaihan@gmail.com>
2019-02-19 17:30:44 +0100

    remove classes for fluentapi

--- src/main/java/com/github/jsontemplate/fluentapi/FluentJsonArrayField.java
+++
@@ -1,8 +0,0 @@
-package com.github.jsontemplate.fluentapi;
-
-public class FluentJsonArrayField implements FluentJsonField {
-
-    public FluentJsonArrayField defaultValue(FluentJsonField value) {
-        return this;
-    }
-}
--- src/main/java/com/github/jsontemplate/fluentapi/FluentJsonObjectField.java
+++
@@ -1,6 +0,0 @@
-package com.github.jsontemplate.fluentapi;
-
-public class FluentJsonObjectField implements FluentJsonField {
-
-
-}
